b86584b1981b1dca1a6864a233c20bebc6c0873f
doumimarwan@ <doumimarwan@gmail.com>
2023-11-06 01:00:44 +0100

    change in fille Play

--- test/src/Component-Qiuz/Play.js
+++ test/src/Component-Qiuz/Play.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Play.css'
 
-const Play = () => {
+const Play = (props) => {
     return (
         <div className='conntainer'>
-            <div className='numpers'><span>10 sec</span><span> 20/20</span></div>
-            <div className='question'><p>What is an IIFE in JavaScript?</p></div>
+            <div className='numpers'><span>10 sec</span><span>{props.numper}/20</span></div>
+            <div className='question'><p>{props.question}</p></div>
             <div className='answer'>
-                <button>Immediately Invoked Function Expression</button>
-                <button>Immediately Invoked Function Expression</button>
-                <button>Immediately Invoked Function Expression</button>
-                <button>Immediately Invoked Function Expression</button>
+                <button onClick={props.answer1} value={props.textvalue1}>{props.showanswer1}</button>
+                <button onClick={props.answer2} value={props.textvalue2}>{props.showanswer2}</button>
+                <button onClick={props.answer3} value={props.textvalue3}>{props.showanswer3}</button>
+                <button onClick={props.answer4} value={props.textvalue4}>{props.showanswer4}</button>
 
             </div>
 
